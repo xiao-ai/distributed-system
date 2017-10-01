0534c455d1ec38ce6b0f1a6a65d7c1554a51dab4
Xiao Ai <ai.x@husky.neu.edu>
2017-10-01 12:21:13 -0700

    added webservice

---
+++ webservice/src/main/java/com/aixiao/client.java
@@ -0,0 +1,7 @@
+package com.aixiao;
+
+public class client {
+  public static void main(String[] argv) {
+    // Please, do not remove this line from file template, here invocation of web service will be inserted  
+  }
+}
